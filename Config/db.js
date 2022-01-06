bea6b2e87186c9d4cbfb578f3b6e045dd1ed63c4
Gagan921 <63442672+Gagan921@users.noreply.github.com>
2022-01-06 05:54:24 +0530

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,1 +1,12 @@
-
+const mongoose = require('mongoose');
+const connectDB= async()=>{
+   const conn = await mongoose.connect(process.env.MONGO_URI,{
+    
+    // }, err => {
+    // if(err) throw err;
+    // console.log(`MongoDB Connected : ${conn.connection.host}`);
+      
+   });
+   console.log(`MongoDB Connected : ${conn.connection.host}`)
+}
+module.exports = connectDB;
